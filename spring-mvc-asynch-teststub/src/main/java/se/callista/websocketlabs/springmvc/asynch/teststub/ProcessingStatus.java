8348bc0fced9c93b5b70c7c8c022f85039d97377
Magnus Larsson <magnus.larsson@callistaenterprise.se>
2014-04-01 21:14:07 +0200

    spring-mvc-asych-teststub: Added XML support by adding jaxb as a dependency

--- spring-mvc-asynch-teststub/src/main/java/se/callista/websocketlabs/springmvc/asynch/teststub/ProcessingStatus.java
+++ spring-mvc-asynch-teststub/src/main/java/se/callista/websocketlabs/springmvc/asynch/teststub/ProcessingStatus.java
@@ -1,10 +1,23 @@
 package se.callista.websocketlabs.springmvc.asynch.teststub;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class ProcessingStatus {
 
+    @XmlElement
     private final String status;
+
+    @XmlElement
     private final int processingTimeMs;
 
+    public ProcessingStatus() {
+        status = "UNKNOWN";
+        processingTimeMs = -1;
+        
+    }
+    
     public ProcessingStatus(String status, int processingTimeMs) {
         this.status = status;
         this.processingTimeMs = processingTimeMs;
